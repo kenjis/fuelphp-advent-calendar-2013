7aa45ce87b51da20fc1dbcdb930f16d330c77ca1
kenjis <kenji.uui@gmail.com>
2013-12-01 18:35:31 +0900

    処理スクリプトの更新

--- review/make-epub.sh
+++ review/make-epub.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd `dirname $0`
-rm -f fuelphp-advent-calendar-2012.epub
-review-epubmaker fuelphp-advent-calendar-2012.yaml
+rm -f fuelphp-advent-calendar-2013.epub
+review-epubmaker fuelphp-advent-calendar-2013.yaml
 
--- review/make-pdf.sh
+++ review/make-pdf.sh
@@ -6,6 +6,6 @@ sh make-re.sh
 # create PDF
 cd `dirname $0`
 #review-preproc --tabwidth=2 --replace *.re
-rm -f fuelphp-advent-calendar-2012.pdf
-review-pdfmaker fuelphp-advent-calendar-2012.yaml
+rm -f fuelphp-advent-calendar-2013.pdf
+review-pdfmaker fuelphp-advent-calendar-2013.yaml
 
