312e615ad4a86f0babb35e1df04c18e8da8ba7bf
kenjis <kenji.uui@gmail.com>
2013-12-16 19:42:18 +0900

    処理スクリプト 記事の切り取りルールの追加

--- libs/BlogArticle.php
+++ libs/BlogArticle.php
@@ -91,8 +91,12 @@
             $html = $dom->find('article', 0)->outertext;
         } elseif ( ! is_null($dom->find('.wp_post', 0))) {
             $html = $dom->find('.wp_post', 0)->outertext;
-        } elseif ( ! is_null($dom->find('.post', 0))) {
+        }
+        // Others
+        elseif ( ! is_null($dom->find('.post', 0))) {
             $html = $dom->find('.post', 0)->outertext;
+        } elseif ( ! is_null($dom->find('#contents', 0))) {
+            $html = $dom->find('#contents', 0)->outertext;
         }
 
         //var_dump($html); exit;
